d073d2269311d29a6ac7f70047f315425377b34f
xaker163 <xaker0163@gmail.com>
2018-06-29 00:36:27 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -2,5 +2,9 @@ public class Calculate {
 
 		public static void main(String[] args) {
 				System.out.println("Calculate...");
+				int first = Integer.valueOf(args[0]);
+				int second = Integer.valueOf(args[1]);
+				int sum = first + second;
+				System.out.println("Sum = " + sum);
 		}
 }
---
+++ Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator{
+	private int result;
+
+	public void add(int ... params){
+		for (int param : params) {
+			this.result += param;
+		}
+	}
+
+	public int getResult() {
+		return this.result;
+	}
+
+	public void cleanResult() {
+		this.result = 0;
+	}
+}
